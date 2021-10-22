d118cc7ab6ec045eaa9d316182a68ac7c8d825ff
tharcissie <tharcissieidufashe@gmail.com>
2021-10-22 20:13:22 +0200

    created list of tracks

--- src/Components/TrackList/TrackList.js
+++ src/Components/TrackList/TrackList.js
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom';
 
 import './TrackList.css';
 
-    class TrackList extends React.Component {
+class TrackList extends React.Component {
 
-        render() {
-    return (
-        <div className="TrackList">
-            {/* <!-- You will add a map method that renders a set of Track components  --> */}
-        </div>
-    );
-}}
+    render() {
+        return (
+            <div className="TrackList">
+                {
+                    this.props.tracks.map(track => {
+                        return <Track track={track}
+                            key={track.id}
+                            onAdd={this.props.onAdd}
+                            isRemoval={this.props.isRemoval}
+                            onRemove={this.props.onRemove} />
+                    })
+                }
+            </div>
+        );
+    }
+}
 
 export default TrackList;
